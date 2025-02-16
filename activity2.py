fed704506fd82ae6a8ff66251b1431c30ac8134c
ShanJoiya <shanjoiya711@gmail.com>
2025-02-16 21:55:32 +0500

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,24 @@
+
+num1 = 45
+num2 = 3
+
+
+print("NUmber 1", num1)
+print("Number 2", num2)
+print("Addition :", num1+num2)
+print("Difference :", num1-num2)
+print("Product :", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square :", num2**2)
+print("Square Root :", num2**0.5)
+
+
+print("Equal ?", num1==num2)
+print("Number greater ?", num1>num2)
+print("Number less ?", num1<num2)
+print("Not equal ?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("Result of given equation is", result)
